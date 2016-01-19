ebab3e2693482aa8f3951613ab3b4a0baf64407c
TOM MEREDITH <tommeredith14@gmail.com>
2016-01-18 23:22:00 -0500

    Update 0.02
    Added input, output, =, ==

--- LinearAlgebra.cpp
+++ LinearAlgebra.cpp
@@ -8,6 +8,13 @@ using std::cout;
 using std::endl;
 
 namespace LinAlg {
+	void Matrix::reset()
+	{
+		for (int i = 0; i < cols; i++)
+			delete a[i];
+		delete a;
+		a = NULL;
+	}
 	Matrix::Matrix() {
 		rows = 1;
 		cols = 1;
@@ -29,21 +36,48 @@ namespace LinAlg {
 	}
 
 	Matrix:: ~Matrix() {
-		for (int i = 0; i < cols; i++)
-			delete a[i];
-		delete a;
+		reset();
 		a = NULL;
 		cout << "all deleted" << endl;
 	}
 	void Matrix::setDimensions(int row, int col) {
 		
-		for (int i = 0; i < cols; i++)
-			delete a[i];
-		delete a;
-		a = NULL;
+		reset();
 		rows = row;
 		cols = col;
 
+	}
+
+	void Matrix::setElement(int row, int col, double val)
+	{
+		a[col][row] = val;
+	}
+
+	double Matrix::getElement(int row, int col) const
+	{
+		return a[col][row];
+	}
+
+	bool Matrix::readIn(std::istream & in)
+	{
+		for (int i = 0; i < rows; i++)
+			for (int j = 0; j < cols; j++)
+				if (!(in >> a[j][i]))
+					return false;
+		return true;
+	}
+
+	void Matrix::output(std::ostream & out)
+	{
+		out << std::fixed << std::setprecision(3);
+		for (int i = 0; i < rows; i++)
+		{
+			for (int j = 0; j < cols; j++)
+				out << std::setw(6) << a[j][i];
+			
+			out << endl;
+		}
+		
 	}
 	
 	int Matrix::getColumns()const {
@@ -51,5 +85,30 @@ namespace LinAlg {
 	}
 	int Matrix::getRows() const {
 		return rows;
+	}
+	Matrix & Matrix::operator=(const Matrix & other)
+	{
+		if (*this == other)
+			return *this;
+		else {
+			this->reset();
+			this->rows = other.rows;
+			this->cols = other.cols;
+			for (int i = 0; i < cols; i++)
+				for (int j = 0; j < rows; j++)
+					this->a[i][j] = other.a[i][j];
+			return *this;
+		}
+		
+	}
+	bool Matrix::operator==(Matrix const & other)const
+	{
+		if (rows != other.rows || cols != other.cols)
+			return false;
+		for (int i = 0; i < cols; i++)
+			for (int j = 0; j < rows; j++)
+				if (a[i][j] != other.a[i][j])
+					return false;
+		return true;
 	}
 }
--- LinearAlgebra.h
+++ LinearAlgebra.h
@@ -3,23 +3,32 @@
 #ifndef LINEARALGEBRA_H
 
 #define LINEARALGEBRA_H
-
+#include<iostream>
+#include<iomanip>
 namespace LinAlg {
 
 	class Matrix {
 
 		int rows, cols;
 		double** a; //a represents the matrix as a 2D array (cols by rows)
+		void reset();
+
 
 	public:
 		Matrix();
 		Matrix(int row, int col);
 		~Matrix();
 		void setDimensions(int row, int col);
+		void setElement(int row, int col, double val);
+		double getElement(int row, int col)const;	
+		bool readIn(std::istream & in);
+		void output(std::ostream & out);
 		
 		int getColumns()const;
 		int getRows() const;
 		
+		bool operator==(Matrix const & other)const;
+		Matrix& operator=(const Matrix & other);
 
 	};
 
